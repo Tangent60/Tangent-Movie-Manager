b3235f4772a560081d0eb574e158b57f38b6dbd0
Nikhil Sinha <nikhil.sinha@hotmail.com>
2016-09-05 23:06:07 +0530

    Complete implemented.

--- WindowsFormsApplication2/DAL.cs
+++ WindowsFormsApplication2/DAL.cs
@@ -1,11 +1,14 @@
-﻿using System.Data;
-using System.Data.SQLite;
+﻿using NLog;
+using System;
 using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
 
 namespace WindowsFormsApplication2
 {
     class DAL
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         private SQLiteConnection _sqlConn;
         private SQLiteCommand _sqlCmd;
         private SQLiteDataAdapter _sqlDataAdapter;
@@ -33,15 +36,33 @@ namespace WindowsFormsApplication2
         {
             SetConnection();
 
-            _sqlConn.Open();
-            _sqlCmd = _sqlConn.CreateCommand();
-            _sqlDataAdapter = new SQLiteDataAdapter(commandText, _sqlConn);
-            _dataSet.Reset();
-            _sqlDataAdapter.Fill(_dataSet);
-            _dataTable = _dataSet.Tables[0];            
-            _sqlConn.Close();
+            try
+            {
+                _sqlConn.Open();
+                _sqlCmd = _sqlConn.CreateCommand();
+                _sqlDataAdapter = new SQLiteDataAdapter(commandText, _sqlConn);
+                _dataSet.Reset();
+                _sqlDataAdapter.Fill(_dataSet);
+                _dataTable = _dataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                logger.Warn(ex);
+                CreateTableMovie();
+                GetData(commandText);
+            }
+            finally
+            {
+                _sqlConn.Close();
+            }
 
             return _dataTable;
+        }
+
+        public void CreateTableMovie()
+        {
+            string query ="CREATE TABLE 'Movie' (	'Name'	TEXT,	'Year'	INTEGER,	'FullPath'	TEXT)";
+            ExecuteQuery(query);
         }
     }
 }
